b11f0a0e1ee048de32e3095cfb70cc9aa947d9a4
Jon <jonhualde3@gmail.com>
2020-04-09 21:55:56 +0200

    Header has been added with Links + little editing on shop design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
-function App(props) {
+function App() {
   return (
+    <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/collections' component={ShopPage} />
       </Switch>
+    </div>
   );
 }
 
