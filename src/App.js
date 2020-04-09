fb4adf0532c6eff64556e21cf343781f2f59df6c
Jon <jonhualde3@gmail.com>
2020-04-09 21:32:32 +0200

    Adding presentation (shop) page with all data

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = (props) => {
-  return (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
-}
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App(props) {
   return (
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/collections' component={ShopPage} />
       </Switch>
   );
 }
