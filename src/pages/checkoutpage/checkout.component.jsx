37695718b717b9fc1a570cee10748eef8c946331
Jon <jonhualde3@gmail.com>
2020-04-17 20:53:43 +0200

    added stripe payment on checkout page (test version)

--- src/pages/checkoutpage/checkout.component.jsx
+++ src/pages/checkoutpage/checkout.component.jsx
@@ -5,7 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartItemsPrice } from '../../redux/cart/cart.selector.js';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component.jsx';
-
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component.jsx';
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, totalPrice }) => (
@@ -32,9 +32,14 @@ const CheckoutPage = ({ cartItems, totalPrice }) => (
                 <CheckoutItem cartItem={cartItem} key={cartItem.id} />
             )
         }
-            <div className='total'>
-                TOTAL: {totalPrice}£
-            </div>
+        <div className='total'>
+            TOTAL: {totalPrice}£
+        </div>
+        <div className='warning-message'>
+            <span>*Please use the following test credit card for payments*</span>
+            <span>4242 4242 4242 4242 -- Exp: 01/20 -- CVV: 123</span>
+        </div>
+        <StripeCheckoutButton price={totalPrice} />
     </div>
 )
 
