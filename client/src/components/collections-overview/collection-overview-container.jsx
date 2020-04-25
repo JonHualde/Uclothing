6f5d3d8d612eae787e07bbc07aa9c974126ba742
Jon <jonhualde3@gmail.com>
2020-04-25 20:56:27 +0200

    express server has been added

---
+++ client/src/components/collections-overview/collection-overview-container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectLoadingState } from '../../redux/shop/shop.selector.js';
+import WithSpinner from '../spinner/spinner.components.jsx';
+import CollectionOverview from './collections-overview.component.jsx';
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: selectLoadingState
+})
+
+const CollectionsOverviewContainer = compose(
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionOverview)
+
+export default CollectionsOverviewContainer;
+
