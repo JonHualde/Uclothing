b0cba5eac476aa3fd57bba2496a42e664292f26b
JonHualde <jonhualde94@gmail.com>
2020-07-19 19:04:22 +0200

    Small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,83 @@
-import React, { useEffect } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCurrentUser } from './redux/user/user.selector';
-import { checkUserSession } from './redux/user/user.actions.js';
+import React, { useEffect } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { checkUserSession } from "./redux/user/user.actions.js";
 
-import { GlobalStyle } from './global.styles';
-//import styled from 'styled-components'; IMPORT STYLED COMPONENTS 
+import { GlobalStyle } from "./global.styles";
+//import styled from 'styled-components'; IMPORT STYLED COMPONENTS
 
-import Homepage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import ContactPage from './pages/contactPage/contactPage.component.jsx'
-import Header from './components/header/header.component.jsx';
-import Footer from './components/footer/footer.component.jsx'
-import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx';
-import CheckoutPage from './pages/checkoutpage/checkout.component.jsx';
+import Homepage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import ContactPage from "./pages/contactPage/contactPage.component.jsx";
+import Header from "./components/header/header.component.jsx";
+import Footer from "./components/footer/footer.component.jsx";
+import SignInAndSignUpPage from "./pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx";
+import CheckoutPage from "./pages/checkoutpage/checkout.component.jsx";
 
-
-const App = ({ checkUserSession, currentUser}) => {
+const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
     checkUserSession();
   }, [checkUserSession]);
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/contact' component={ContactPage} />
-        <Route exact path='/sign-in' 
-          render={ () => currentUser 
-          ? ( <Redirect to='/' /> ) 
-          : ( <SignInAndSignUpPage />) 
-        }/>
-        <Route exact path='/checkout' component={CheckoutPage}/>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route
+          exact
+          path="/sign-in"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+        <Route exact path="/checkout" component={CheckoutPage} />
       </Switch>
       <Footer />
     </div>
   );
-}
+};
 
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
-})
+  currentUser: selectCurrentUser,
+});
 
-const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession())
-})
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
 
 // ---- EXAMPLE OF STYLED DIV ----- //
 // const Text = styled.div`
 //   font-size: 64px;
 // `;
 
-// Implement it this way into the class: 
+// Implement it this way into the class:
 // <div>
-//   <Text> I AM HERE </Text> 
+//   <Text> I AM HERE </Text>
 // </div>
 
 // ------------- WE ALSO CAN PASS PROPS INTO THE ELEMENT TO AUTOMATICALLY RENDER A PARTICULAR STYLE. ------------
 
 // <div>
-//   <Text isActive={true}> I AM HERE </Text> 
+//   <Text isActive={true}> I AM HERE </Text>
 // </div>
 
 // const Text = styled.div`
 //   font-size: 64px;
 //   border: ${ ({ isActive }) => isActive ? '2px solid black' : '3px dashed green' };
 // `;
-// COMPONENT IS NOW ABLE TO LEVERAGE PROPS! 
+// COMPONENT IS NOW ABLE TO LEVERAGE PROPS!
 
-
-// --------- FUNCTION TO PUSH DATA TO FIREBASE IF NEEDED --------- // 
+// --------- FUNCTION TO PUSH DATA TO FIREBASE IF NEEDED --------- //
 // addCollectionAndDocuments('collections', collections.map(
 //        ({title, items})  =>  ({title, items}) ));
-
 
 // -------------------- Quick recap on Hooks --------------------
 // ComponentDidMount
@@ -93,7 +90,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(App);
 //     console.log('I just mounted!');
 // }, [])
 
-
 // ComponentWillUnmount
 // ----- Class -----
 // componentWillUnmount() {
@@ -103,7 +99,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(App);
 // useEffect(() => {
 //     return () => console.log('I am unmounting');
 // }, [])
-
 
 // ComponentWillReceiveProps
 // ----- Class -----
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,27 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 
-import { connect } from 'react-redux';
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions.js';
+import { connect } from "react-redux";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions.js";
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview-container.jsx';
-import CollectionPageContainer from '../../pages/collection/collection.container.jsx';
-
+import CollectionsOverviewContainer from "../../components/collections-overview/collection-overview-container.jsx";
+import CollectionPageContainer from "../../pages/collection/collection.container.jsx";
 
 const ShopPage = ({ match, fetchCollectionsStart }) => {
-    useEffect(() => {
-        fetchCollectionsStart();
-    }, [fetchCollectionsStart])
-    return(
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-        </div>
-        )
-    }
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
-const mapDispatchToPros = dispatch => ({
-    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-})
+const mapDispatchToPros = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
 
 export default connect(null, mapDispatchToPros)(ShopPage);
