c93cf5f283a473bd0c240e059dfc0f4e21796f74
JonHualde <jonhualde94@gmail.com>
2020-11-05 20:06:21 +0000

    Removing credentials from the website

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -8,17 +8,18 @@ const Footer = () => {
             <div className='upper_part'>
                 <div className='upper_text'>
                     <a href='/contact'>Contact</a>
+                    {/* <p>|</p> */}
+                    {/* <a href='https://jonhualde.github.io/' target='_blank'>Website of the owner</a> */}
                     <p>|</p>
-                    <a href='https://jonhualde.github.io/' target='_blank'>Website of the owner</a>
+                    <a href='/' target='_blank' rel="noopener noreferrer">Legal notice</a>
                     <p>|</p>
-                    <a href='/' target='_blank'>Legal notice</a>
-                    <p>|</p>
-                    <a href='/' target='_blank'>Latest clothin trends</a>
+                    <a href='/' target='_blank' rel="noopener noreferrer">Latest clothin trends</a>
                 </div>
             </div>
             <div className='lower_part'>
                 <div className='lower_text'>
-                    <p>Made by <a href='https://github.com/JonHualde' target='_blank'>Jon Hualde</a> with passion & React.JS </p>
+                    <p>Made by me with passion & React.JS </p>
+                    {/* <p>Made by <a href='https://github.com/JonHualde' target='_blank'>Jon Hualde</a> with passion & React.JS </p> */}
                 </div>
             </div>
         </div>
